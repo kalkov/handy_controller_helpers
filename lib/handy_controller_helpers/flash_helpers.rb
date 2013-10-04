9c5af3ee940cde31a540d9dc2bd49199daef5727
Vassil Kalkov <vassilkalkov@gmail.com>
2013-10-04 11:11:04 +0300

    Remove dots in flash helpers

--- lib/handy_controller_helpers/flash_helpers.rb
+++ lib/handy_controller_helpers/flash_helpers.rb
@@ -2,19 +2,19 @@ module HandyControllerHelpers
   module FlashHelpers
 
     def flash_unauthorized?
-      flash[:alert].should =~ /You are not authorized to access this page./i
+      flash[:alert].should =~ /You are not authorized to access this page/i
     end
 
     def flash_updated?
-      flash[:notice].should =~ /was successfully updated./i
+      flash[:notice].should =~ /was successfully updated/i
     end
 
     def flash_created?
-      flash[:notice].should =~ /was successfully created./i
+      flash[:notice].should =~ /was successfully created/i
     end
 
     def flash_destroyed?
-      flash[:notice].should =~ /was successfully destroyed./i
+      flash[:notice].should =~ /was successfully destroyed/i
     end
 
     def flash_recovered?
